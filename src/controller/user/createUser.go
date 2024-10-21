8249dc7da39bba1360592a8667e8bddcb9cd6f94
Reis567 <matheusdosreis9@gmail.com>
2024-10-21 07:57:21 -0300

    👌 IMPROVE: request to create user

--- src/controller/user/createUser.go
+++ src/controller/user/createUser.go
@@ -1,7 +1,21 @@
 package user
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"meu-novo-projeto/src/configuration/rest_err"
+	"meu-novo-projeto/src/controller/model/request"
+
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context) {
-    // lógica para criar um usuário
+   var userRequest request.UserRequest
+   if err := c.ShouldBindJSON(&userRequest); err != nil{
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, erro=%s",err)
+		)
+		c.JSON(restErr.Code,restErr)
+		return
+   }
+   fmt.Println(userRequest)
 }
