6cdd4e1ade4af5626ef62787ff2b4cc1de392793
Reis567 <matheusdosreis9@gmail.com>
2024-10-30 08:03:52 -0300

    👌 IMPROVE: all fields in convert_domain_toresponse

--- src/view/convert_domain_toresponse.go
+++ src/view/convert_domain_toresponse.go
@@ -5,6 +5,15 @@ import (
 	"meu-novo-projeto/src/model"
 )
 
+// convert_domain_toresponse converte UserDomainInterface para UserResponse
 func convert_domain_toresponse(userDomain model.UserDomainInterface) response.UserResponse {
-	return response.UserResponse{ID: "",}
+	return response.UserResponse{
+		ID:         userDomain.GetID(),
+		Email:      userDomain.GetEmail(),
+		FirstName:  userDomain.GetFirstName(),
+		LastName:   userDomain.GetLastName(),
+		Age:        userDomain.GetAge(),
+		CreatedAt:  userDomain.GetCreatedAt(),
+		UpdatedAt:  userDomain.GetUpdatedAt(),
+	}
 }
