3f6a2f917da93f63ab7acfe18d512435ecaf7c11
Reis567 <matheusdosreis9@gmail.com>
2024-11-01 07:35:17 -0300

    👌 IMPROVE: main.go db connection and trusted proxies

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 
+	"meu-novo-projeto/src/configuration/database"
 	"meu-novo-projeto/src/configuration/logger"
 	"meu-novo-projeto/src/controller/routes"
 	"meu-novo-projeto/src/middleware"
@@ -14,30 +15,41 @@ import (
 )
 
 func main() {
-    logger.Info("About to start application")
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatalf("Erro ao carregar o arquivo .env: %v", err)
-    }
+	// Mensagem de log inicial
+	logger.Info("Starting application...")
 
-    // Usar as variáveis de ambiente
-    appPort := os.Getenv("APP_PORT")
-    if appPort == "" {
-        log.Fatal("A porta da aplicação (APP_PORT) não está definida no arquivo .env")
-    }
+	// Carregar variáveis de ambiente
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Erro ao carregar o arquivo .env: %v", err)
+	}
 
-    // Configurar o servidor Gin
-    router := gin.Default()
+	// Conectar ao banco de dados
+	_, err = database.ConnectDatabase()
+	if err != nil {
+		log.Fatalf("Erro ao conectar ao banco de dados: %v", err)
+	}
+	log.Println("Conexão com o banco de dados estabelecida com sucesso!")
 
-    // Aplicar middleware de erros
-    router.Use(middleware.ErrorHandlingMiddleware())
+	// Usar a porta da aplicação definida nas variáveis de ambiente
+	appPort := os.Getenv("APP_PORT")
+	if appPort == "" {
+		log.Fatal("A porta da aplicação (APP_PORT) não está definida no arquivo .env")
+	}
 
-    // Inicializar rotas
-    api := router.Group("/api/v1")
-    routes.InitRoutes(api)
+	// Configurar o servidor Gin
+	router := gin.Default()
+    router.SetTrustedProxies([]string{"127.0.0.1"})
 
-    // Rodar a aplicação e tratar erro de inicialização
-    if err := router.Run(fmt.Sprintf(":%s", appPort)); err != nil {
-        log.Fatalf("Erro ao iniciar o servidor: %v", err)
-    }
+	// Aplicar middleware de erros
+	router.Use(middleware.ErrorHandlingMiddleware())
+
+	// Inicializar rotas
+	api := router.Group("/api/v1")
+	routes.InitRoutes(api)
+
+	// Rodar a aplicação e tratar erro de inicialização
+	if err := router.Run(fmt.Sprintf(":%s", appPort)); err != nil {
+		log.Fatalf("Erro ao iniciar o servidor: %v", err)
+	}
 }
