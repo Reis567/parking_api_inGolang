cdaf6ffc939c6cd019b825f1ae22517c747a539d
Reis567 <matheusdosreis9@gmail.com>
2024-11-02 10:14:46 -0300

    👌 IMPROVE: User repository

--- src/model/repository/user_repository.go
+++ src/model/repository/user_repository.go
@@ -1,5 +1,10 @@
 package repository
 
+import (
+	"meu-novo-projeto/src/configuration/rest_err"
+	"meu-novo-projeto/src/model"
+)
+
 type UserRepository interface {
-	
+	CreateUser(userDomain model.UserDomainInterface) (model.UserDomainInterface ,*rest_err.RestErr)
 }
