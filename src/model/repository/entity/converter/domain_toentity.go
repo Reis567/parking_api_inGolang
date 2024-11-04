e65f58f584065928796652164abaafce576613c1
Reis567 <matheusdosreis9@gmail.com>
2024-11-04 08:07:46 -0300

    👌 IMPROVE: ConvertDomainToEntity

---
+++ src/model/repository/entity/converter/domain_toentity.go
@@ -0,0 +1,20 @@
+package entity
+
+import (
+	"meu-novo-projeto/src/model"
+	"meu-novo-projeto/src/model/repository/entity"
+)
+
+// ConvertDomainToEntity converte um UserDomain para UserEntity
+func ConvertDomainToEntity(domain model.UserDomainInterface) entity.UserEntity {
+	return entity.UserEntity{
+		ID:        domain.GetID(),
+		FirstName: domain.GetFirstName(),
+		LastName:  domain.GetLastName(),
+		Email:     domain.GetEmail(),
+		Password:  domain.GetPassword(),
+		CreatedAt: domain.GetCreatedAt(),
+		UpdatedAt: domain.GetUpdatedAt(),
+		Age:       domain.GetAge(),
+	}
+}
