1a3769edd9a9b0e14fcd0e8d2eed06433f654177
Reis567 <matheusdosreis9@gmail.com>
2024-11-13 07:10:03 -0300

    👌 IMPROVE: remove id atribuition from create user

--- src/model/service/create_user.go
+++ src/model/service/create_user.go
@@ -1,38 +1,32 @@
 package service
 
 import (
-	"fmt"
 	"meu-novo-projeto/src/configuration/logger"
 	"meu-novo-projeto/src/configuration/rest_err"
 	"meu-novo-projeto/src/model"
-	"time"
-
 	"go.uber.org/zap"
+	"time"
 )
 
 // CreateUser cria um novo usuário no sistema
 func (s *userDomainService) CreateUserService(user model.UserDomainInterface) (model.UserDomainInterface, *rest_err.RestErr) {
 	logger.Info("Init CreateUser model", zap.String("journey", "Create user"))
 
-	// Atribuir ID e datas
-	user.(*model.UserDomain).ID = generateID()
+	// Atribuir datas (ID será gerenciado automaticamente pelo GORM)
 	user.(*model.UserDomain).CreatedAt = time.Now().Format(time.RFC3339)
 	user.(*model.UserDomain).UpdatedAt = time.Now().Format(time.RFC3339)
 
 	// Encriptar a senha
 	user.(*model.UserDomain).EncryptPassword()
-	userDomainRepository , err := s.userRepository.CreateUser(user)
+
+	// Salvar o usuário no repositório
+	userDomainRepository, err := s.userRepository.CreateUser(user)
 	if err != nil {
 		logger.Error("Erro ao salvar usuário no banco de dados", zap.Error(err))
 		return nil, err
 	}
 
-	// Log e retorno do usuário criado (simulação)
-	logger.Info("Usuário criado com sucesso", zap.String("user_id", user.GetID()))
+	// Log de sucesso
+	logger.Info("Usuário criado com sucesso", zap.Uint("user_id", userDomainRepository.GetID()))
 	return userDomainRepository, nil
-}
-
-// Exemplo de função para geração de ID
-func generateID() string {
-	return fmt.Sprintf("%d", time.Now().UnixNano())
 }
