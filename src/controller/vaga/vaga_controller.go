82b1e7824197e3d801c8f372435b9fe37a964ca8
Reis567 <matheusdosreis9@gmail.com>
2024-12-11 07:40:26 -0300

    👌 IMPROVE: NewVagaControllerInterface

---
+++ src/controller/vaga/vaga_controller.go
@@ -0,0 +1,27 @@
+package vaga
+
+import (
+	"meu-novo-projeto/src/model/service"
+	"github.com/gin-gonic/gin"
+)
+
+// NewVagaControllerInterface cria uma nova instância de VagaControllerInterface
+func NewVagaControllerInterface(serviceInterface service.VagaDomainService) VagaControllerInterface {
+	return &vagaControllerInterface{
+		service: serviceInterface,
+	}
+}
+
+// VagaControllerInterface define os métodos do controlador de vaga
+type VagaControllerInterface interface {
+	CreateVaga(c *gin.Context)
+	FindVagaByID(c *gin.Context)
+	FindAllVagas(c *gin.Context)
+	UpdateVaga(c *gin.Context)
+	DeleteVaga(c *gin.Context)
+}
+
+// vagaControllerInterface implementa VagaControllerInterface
+type vagaControllerInterface struct {
+	service service.VagaDomainService
+}
