9efbb39c4cfd8b093f156f233dd34b1fbe0294c2
Reis567 <matheusdosreis9@gmail.com>
2024-12-11 07:46:21 -0300

    📦 NEW: FindVagaByID

---
+++ src/controller/vaga/findVaga.go
@@ -0,0 +1,25 @@
+package vaga
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+// FindVagaByID busca uma vaga pelo ID
+func (vc *vagaControllerInterface) FindVagaByID(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "ID inválido"})
+		return
+	}
+
+	vaga, errRes := vc.service.FindVagaByIDService(uint(id))
+	if errRes != nil {
+		c.JSON(errRes.Code, gin.H{"message": errRes.Message})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"vaga": vaga})
+}
+
