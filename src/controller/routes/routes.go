f9cb37079c9514c8a9b83c73d3b682be2baee854
Matheus dos reis <matheusdosreis9@gmail.com>
2024-12-27 16:50:32 -0300

    👌 IMPROVE: vehicleRoutes

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -3,11 +3,12 @@ package routes
 import (
 	"meu-novo-projeto/src/controller/user"
 	"meu-novo-projeto/src/controller/vaga"
+	"meu-novo-projeto/src/controller/veiculo" // Novo import para o controlador de veículo
 	"meu-novo-projeto/src/middleware"
 	"github.com/gin-gonic/gin"
 )
 
-func InitRoutes(r *gin.RouterGroup, userController user.UserControllerInterface, vagaController vaga.VagaControllerInterface) {
+func InitRoutes(r *gin.RouterGroup, userController user.UserControllerInterface, vagaController vaga.VagaControllerInterface, veiculoController veiculo.VehicleControllerInterface) {
 	// Rotas de usuário
 	userRoutes := r.Group("/users")
 	{
@@ -35,6 +36,20 @@ func InitRoutes(r *gin.RouterGroup, userController user.UserControllerInterface,
 			vagaRoutes.GET("/", vagaController.FindAllVagas)
 			vagaRoutes.PUT("/:id", vagaController.UpdateVaga)
 			vagaRoutes.DELETE("/:id", vagaController.DeleteVaga)
+		}
+	}
+
+	// Rotas de veículo
+	vehicleRoutes := r.Group("/veiculos")
+	{
+		// Aplica o middleware AuthMiddleware às rotas protegidas de veículos
+		vehicleRoutes.Use(middleware.AuthMiddleware())
+		{
+			vehicleRoutes.POST("/", veiculoController.CreateVehicle)
+			vehicleRoutes.GET("/:id", veiculoController.FindVehicleByID)
+			vehicleRoutes.GET("/", veiculoController.FindAllVehicles)
+			vehicleRoutes.PUT("/:id", veiculoController.UpdateVehicle)
+			vehicleRoutes.DELETE("/:id", veiculoController.DeleteVehicle)
 		}
 	}
 }
