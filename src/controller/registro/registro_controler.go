47725621e86aeff45303d097c3ff11ef64191c8f
Reis567 <matheusdosreis9@gmail.com>
2025-01-08 18:02:03 -0300

    👌 IMPROVE: RegistroControllerInterface

---
+++ src/controller/registro/registro_controler.go
@@ -0,0 +1,27 @@
+package registro
+
+import (
+	"meu-novo-projeto/src/model/service"
+	"github.com/gin-gonic/gin"
+)
+
+// NewRegistroControllerInterface cria uma nova instância de RegistroControllerInterface
+func NewRegistroControllerInterface(serviceInterface service.RegistroEstacionamentoDomainService) RegistroControllerInterface {
+	return &registroControllerInterface{
+		service: serviceInterface,
+	}
+}
+
+// RegistroControllerInterface define os métodos do controlador de registro
+type RegistroControllerInterface interface {
+	CreateRegistro(c *gin.Context)
+	FindRegistroByID(c *gin.Context)
+	FindAllRegistros(c *gin.Context)
+	UpdateRegistro(c *gin.Context)
+	DeleteRegistro(c *gin.Context)
+}
+
+// registroControllerInterface implementa RegistroControllerInterface
+type registroControllerInterface struct {
+	service service.RegistroEstacionamentoDomainService
+}
