42b49d08129948e900a82998968a2ab02ba2d2d9
Reis567 <matheusdosreis9@gmail.com>
2025-01-08 18:02:49 -0300

    👌 IMPROVE: FindRegistroByID

---
+++ src/controller/registro/findRegistro.go
@@ -0,0 +1,24 @@
+package registro
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func (rc *registroControllerInterface) FindRegistroByID(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "ID inválido", "error": err.Error()})
+		return
+	}
+
+	// Chamar o serviço para buscar o registro
+	registro, serviceErr := rc.service.FindRegistroByIDService(uint(id))
+	if serviceErr != nil {
+		c.JSON(serviceErr.Code, gin.H{"message": serviceErr.Message})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"registro": registro})
+}
