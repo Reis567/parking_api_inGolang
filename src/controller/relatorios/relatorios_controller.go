c79246c9c1b68a8b69a3fb1cc89e22a25754bbdf
Reis567 <matheusdosreis9@gmail.com>
2025-01-20 06:57:47 -0300

    👌 IMPROVE: RelatoriosController

---
+++ src/controller/relatorios/relatorios_controller.go
@@ -0,0 +1,25 @@
+package relatorios
+
+import (
+	"meu-novo-projeto/src/model/service"
+	"github.com/gin-gonic/gin"
+)
+
+// NewRelatoriosController cria uma nova instância de RelatoriosController
+func NewRelatoriosController(serviceInterface service.RelatoriosService) RelatoriosController {
+	return &relatoriosController{
+		service: serviceInterface,
+	}
+}
+
+// RelatoriosController define os métodos do controlador de relatórios
+type RelatoriosController interface {
+	CalcularReceita(c *gin.Context)
+	CalcularOcupacaoAtual(c *gin.Context)
+	VeiculosMaisFrequentes(c *gin.Context)
+}
+
+// relatoriosController implementa RelatoriosController
+type relatoriosController struct {
+	service service.RelatoriosService
+}
